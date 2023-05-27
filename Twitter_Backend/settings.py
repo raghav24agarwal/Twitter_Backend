08418a174e2d0c53d5d5b234cfe7fafe6588c72f
raghav24agarwal <rsag732@gmail.com>
2023-05-27 17:47:17 +0530

    Removed info.py

--- Twitter_Backend/settings.py
+++ Twitter_Backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Manual Imports 
-from .info import *
+# from .info import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
